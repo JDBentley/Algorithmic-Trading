0f97bb478d7a1f5c7193e4519ad192bd315e43f4
JDBentley <SneakyEskimo@outlook.com>
2020-12-20 15:42:54 -0500

    Created data frame

--- equalweight.py
+++ equalweight.py
@@ -16,3 +16,10 @@ symbol = 'AAPL'
 api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
 data = requests.get(api_url).json()
 print(data)
+
+data ['latestPrice']
+data ['marketCap']
+
+my_columns = ['Ticker', 'Price', 'Market Capitalization', 'Number of Shares to Buy']
+final_dataframe = pd.DataFrame(columns = my_columns)
+print(final_dataframe)
