0aabd681662218d363301029dbe3e890cf4068e2
JDBentley <SneakyEskimo@outlook.com>
2020-12-20 15:55:47 -0500

    Single Call complete

--- equalweight.py
+++ equalweight.py
@@ -17,9 +17,11 @@ api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_C
 data = requests.get(api_url).json()
 print(data)
 
+# parsing API call
 data ['latestPrice']
 data ['marketCap']
 
+# adding stocks to data frames
 my_columns = ['Ticker', 'Price', 'Market Capitalization', 'Number of Shares to Buy']
 final_dataframe = pd.DataFrame(columns = my_columns)
 print(final_dataframe)
@@ -35,3 +37,20 @@ final_dataframe = final_dataframe.append(
     ignore_index = True
 )
 print(final_dataframe)
+
+#looping through the tickers in our list of stocks
+final_dataframe = pd.DataFrame(columns = my_columns)
+for symbol in stocks['Ticker']:
+    data = requests.get(api_url).json()
+    final_dataframe = final_dataframe.append(
+        pd.Series(
+            [symbol,
+            data['latestPrice'],
+            data['marketCap'],
+            'N/A'],
+            index = my_columns
+        ),
+        ignore_index= True
+    )
+print(final_dataframe)
+
